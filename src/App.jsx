7bf24c431373dd044213aba606102f2d6284b396
Aa-thomas <aarthomas01@gmail.com>
2023-01-28 18:38:49 -0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './styles/App.css';
+import PathVisualizer from './components/PathVisualizer';
+
+function App() {
+	return (
+		<div className="App">
+			<PathVisualizer />
+		</div>
+	);
+}
+
+export default App;
---
+++ src/components/PathVisualizer.jsx
@@ -0,0 +1,33 @@
+import { useEffect, useState } from 'react';
+import Node from './Node';
+
+export default function PathVisualizer() {
+	const [nodes, setNodes] = useState([]);
+
+	useEffect(() => {
+		const nodesArray = [];
+
+		for (let row = 0; row < 50; row++) {
+			const currentRow = [];
+			for (let col = 0; col < 15; col++) {
+				currentRow.push([]);
+			}
+			nodesArray.push(currentRow);
+		}
+		setNodes(nodesArray);
+	}, []);
+
+	return (
+		<div className="grid">
+			{nodes.map((row, rowIndex) => {
+				return (
+					<div className="column" key={rowIndex}>
+						{row.map((node, nodeIndex) => {
+							return <Node key={nodeIndex} />;
+						})}
+					</div>
+				);
+			})}
+		</div>
+	);
+}
