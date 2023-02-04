b7d29236a32c7aec55a879faf84a3f3741aca2df
Aa-thomas <aarthomas01@gmail.com>
2023-02-04 13:19:28 -0500

    rename: `Node` -> `GridNode`

---
+++ src/components/GridNode.jsx
@@ -0,0 +1,8 @@
+export default function GridNode({ isOrigin, isTarget }) {
+	let nodeClassName;
+	if (isTarget) nodeClassName = 'node-target';
+	else if (isOrigin) nodeClassName = 'node-origin';
+	else nodeClassName = '';
+
+	return <div className={`node ${nodeClassName}`}></div>;
+}
